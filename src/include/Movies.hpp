a0549ba05a5aecffe2bd775357dbbcd2713bd0a0
mpflynnx <mpflynnx@gmail.com>
2024-10-14 14:05:27 +0000

    fixes #1 Added readme, main.cpp, class specification and implementation files. Checked IDE compile and debugging.

---
+++ src/include/Movies.hpp
@@ -0,0 +1,23 @@
+#ifndef _MOVIES_H_
+#define _MOVIES_H_
+#include <string>
+#include <iostream>
+
+class Movies
+{
+private:
+
+public:
+
+    // Constructor prototype
+    Movies();
+
+    // Copy Constructor prototype
+    // Movie(const Movie &source);
+   
+    // Destructor prototype
+    ~ Movies();
+
+};
+
+#endif // _MOVIES_H_
