0b2d278063a444a84e80470d774095ab61845d2a
Matthew Flynn <90197912+mpflynnx@users.noreply.github.com>
2024-10-14 14:07:36 +0100

    fixes #1 (#2)
    Added readme, main.cpp, class specification and implementation files.
    Checked IDE compile and debugging.

---
+++ src/Movies.cpp
@@ -0,0 +1,1 @@
+#include "include/Movies.hpp"
---
+++ src/include/Movie.hpp
@@ -0,0 +1,23 @@
+#ifndef _MOVIE_H_
+#define _MOVIE_H_
+#include <string>
+#include <iostream>
+
+class Movie
+{
+private:
+
+public:
+
+    // Constructor prototype
+    Movie();
+
+    // Copy Constructor prototype
+    // Movie(const Movie &source);
+   
+    // Destructor prototype
+    ~ Movie();
+
+};
+
+#endif // _MOVIE_H_
