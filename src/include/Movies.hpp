9e4696c28f8521381523007ee5c4c0c802025fee
mpflynnx <mpflynnx@gmail.com>
2024-10-15 10:36:38 +0000

    Started on Movies class specification and implementation. fixes #5

--- src/include/Movies.hpp
+++ src/include/Movies.hpp
@@ -2,21 +2,35 @@
 #define _MOVIES_H_
 #include <string>
 #include <iostream>
+#include <vector>
+#include "Movie.hpp"
 
 class Movies
 {
 private:
+    std::vector<Movie> m_movies;
 
 public:
 
     // Constructor prototype
-    Movies();
+    Movies() {}; 
 
     // Copy Constructor prototype
     // Movie(const Movie &source);
-   
-    // Destructor prototype
-    ~ Movies();
+
+    void add_movie(const Movie& movie)
+    {
+        m_movies.push_back(movie);
+    }
+
+    void print() const
+    {
+        for (const auto& movie : m_movies)
+        {
+            movie.print();
+        }
+
+    }
 
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 #include "include/Movie.hpp"
+#include "include/Movies.hpp"
 
 int main ()
 {
 
     // Movie big(); // not wanting this type of instantiation
-    Movie big("Big", "12A", 1);
-    Movie big2("Big2", "12A", 1);
+    // Movie big("Big", "12A", 1);
+    // Movie big2("Big2", "12A", 1);
+
+    // big2.set_name("Bigger2");
+    // std::cout << big2.get_name() << std::endl;
+    // big2.set_watch_count(big2.get_watch_count() + 1);
+    // std::cout << big2.get_watch_count() << std::endl;
+
+    
+    // Movie mermaid("Mermaid", "PG");
+    // std::cout << mermaid.get_name();
+
+    Movies my_movies;
+    my_movies.add_movie(Movie("Big", "12A", 1));
+    my_movies.add_movie(Movie("Bigger", "PG", 1));
+
+    my_movies.print();  
 
     return 0;
 }
