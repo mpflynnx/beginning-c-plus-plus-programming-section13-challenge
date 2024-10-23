78f997b4d442033116eb329c9e4fee8a1888e14b
Matthew Flynn <90197912+mpflynnx@users.noreply.github.com>
2024-10-23 10:34:42 +0100

    Added Movies member function: add_movie() (#8)
    fixes #7

--- src/Movies.cpp
+++ src/Movies.cpp
@@ -1,2 +1,23 @@
 #include "include/Movies.hpp"
+#include "include/Movie.hpp"
+#include <algorithm> // find_if
 
+void Movies::add_movie(const Movie& movie)
+{
+    std::cout << "************************\n";
+    std::cout << "Adding " << movie.m_name << " to vector.\n";
+
+    // search vector for an existing Movie m_name, using stl find-if and a lambda function
+    auto iter = std::find_if(m_movies.begin(), m_movies.end(), [&movie](const Movie& existingMovie) {
+        return existingMovie.m_name == movie.m_name;}); // true if name exists
+
+    if (iter == m_movies.end())
+    {
+        std::cout << "Movie " << movie.m_name << " added to vector.\n";
+        m_movies.push_back(movie);
+    }
+    else
+    {
+        std::cout << "Movie " << movie.m_name << " already exists in vector.\n";
+    }
+}
--- src/include/Movie.hpp
+++ src/include/Movie.hpp
@@ -6,11 +6,15 @@
 class Movie
 {
 private:
-    std::string m_name;
     std::string m_age_rating; // U, PG, 12, 12A, 15, 18
     int m_watch_count;
 
+protected:
+
+
 public:
+
+    std::string m_name; // for use with Movies
 
     void set_name(std::string name)
     {
